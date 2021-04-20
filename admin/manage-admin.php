373ee0159b6d5eeed9c20043b61268b2a6af71e0
samir <samirbabayevdir@gmail.com>
2021-04-20 15:12:56 +0400

    admin/ manage pages layout

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -7,7 +7,42 @@ include('partials/menu.php');
       <div class="wrapper">
         <h1>Manage Admin</h1>
         <br><br>
-
+        <a href="add-admin.php" class="btn-primary">Add Admin</a>
+        <table class="tbl-full">
+          <tr>
+            <th>S.N</th>
+            <th>Full Name</th>
+            <th>Username</th>
+            <th>Actions</th>
+          </tr>
+          <tr>
+            <td>1.</td>
+            <td>Sam</td>
+            <td>sam</td>
+            <td>
+              <a href="" class="btn-secondary">Update Admin</a>
+              <a href="" class="btn-danger">Delete Admin</a>
+            </td>
+          </tr>
+          <tr>
+            <td>2.</td>
+            <td>Sam</td>
+            <td>sam</td>
+            <td>
+              <a href="" class="btn-secondary">Update Admin</a>
+              <a href="" class="btn-danger">Delete Admin</a>
+            </td>
+          </tr>
+          <tr>
+            <td>3.</td>
+            <td>Sam</td>
+            <td>sam</td>
+            <td>
+              <a href="" class="btn-secondary">Update Admin</a>
+              <a href="" class="btn-danger">Delete Admin</a>
+            </td>
+          </tr>
+        </table>
       </div>
     </div>
   </div>
--- admin/manage-food.php
+++ admin/manage-food.php
@@ -7,7 +7,42 @@ include('partials/menu.php');
       <div class="wrapper">
         <h1>Manage Food</h1>
         <br><br>
-
+        <a href="" class="btn-primary">Add Food</a>
+        <table class="tbl-full">
+          <tr>
+            <th>S.N</th>
+            <th>Full Name</th>
+            <th>Username</th>
+            <th>Actions</th>
+          </tr>
+          <tr>
+            <td>1.</td>
+            <td>Sam</td>
+            <td>sam</td>
+            <td>
+              <a href="" class="btn-secondary">Update Admin</a>
+              <a href="" class="btn-danger">Delete Admin</a>
+            </td>
+          </tr>
+          <tr>
+            <td>2.</td>
+            <td>Sam</td>
+            <td>sam</td>
+            <td>
+              <a href="" class="btn-secondary">Update Admin</a>
+              <a href="" class="btn-danger">Delete Admin</a>
+            </td>
+          </tr>
+          <tr>
+            <td>3.</td>
+            <td>Sam</td>
+            <td>sam</td>
+            <td>
+              <a href="" class="btn-secondary">Update Admin</a>
+              <a href="" class="btn-danger">Delete Admin</a>
+            </td>
+          </tr>
+        </table>
       </div>
     </div>
   </div>
