f9e69114c8f93b9ae407f3b0007f200460c5dcc7
samir <samirbabayevdir@gmail.com>
2021-04-20 20:07:12 +0400

    admin/ orders - manage, update

--- admin/manage-order.php
+++ admin/manage-order.php
@@ -1,52 +1,109 @@
-<?php
-include('partials/menu.php');
-?>
-<section class="menu content">
+<?php include('partials/menu.php'); ?>
+
+<div class="main-content">
   <div class="wrapper">
-    <div class="main-content">
-      <div class="wrapper">
-        <h1>Manage Order</h1>
-        <br><br>
-        <a href="" class="btn-primary">Add Order</a>
-        <table class="tbl-full">
+    <h1>Manage Order</h1>
+
+    <br /><br /><br />
+
+    <?php
+    if (isset($_SESSION['update'])) {
+      echo $_SESSION['update'];
+      unset($_SESSION['update']);
+    }
+    ?>
+    <br><br>
+
+    <table class="tbl-full">
+      <tr>
+        <th>S.N.</th>
+        <th>Food</th>
+        <th>Price</th>
+        <th>Qty.</th>
+        <th>Total</th>
+        <th>Order Date</th>
+        <th>Status</th>
+        <th>Customer Name</th>
+        <th>Contact</th>
+        <th>Email</th>
+        <th>Address</th>
+        <th>Actions</th>
+      </tr>
+
+      <?php
+      //Get all the orders from database
+      $sql = "SELECT * FROM orders ORDER BY id DESC"; // DIsplay the Latest Order at First
+      //Execute Query
+      $res = mysqli_query($conn, $sql);
+      //Count the Rows
+      $count = mysqli_num_rows($res);
+
+      $sn = 1; //Create a Serial Number and set its initail value as 1
+
+      if ($count > 0) {
+        //Order Available
+        while ($row = mysqli_fetch_assoc($res)) {
+          //Get all the order details
+          $id = $row['id'];
+          $food = $row['food'];
+          $price = $row['price'];
+          $qty = $row['qty'];
+          $total = $row['total'];
+          $order_date = $row['order_date'];
+          $status = $row['status'];
+          $customer_name = $row['customer_name'];
+          $customer_contact = $row['customer_contact'];
+          $customer_email = $row['customer_email'];
+          $customer_address = $row['customer_address'];
+
+      ?>
+
           <tr>
-            <th>S.N</th>
-            <th>Full Name</th>
-            <th>Username</th>
-            <th>Actions</th>
-          </tr>
-          <tr>
-            <td>1.</td>
-            <td>Sam</td>
-            <td>sam</td>
+            <td><?php echo $sn++; ?>. </td>
+            <td><?php echo $food; ?></td>
+            <td><?php echo $price; ?></td>
+            <td><?php echo $qty; ?></td>
+            <td><?php echo $total; ?></td>
+            <td><?php echo $order_date; ?></td>
+
             <td>
-              <a href="" class="btn-secondary">Update Admin</a>
-              <a href="" class="btn-danger">Delete Admin</a>
+              <?php
+              // Ordered, On Delivery, Delivered, Cancelled
+
+              if ($status == "Ordered") {
+                echo "<label>$status</label>";
+              } elseif ($status == "On Delivery") {
+                echo "<label style='color: orange;'>$status</label>";
+              } elseif ($status == "Delivered") {
+                echo "<label style='color: green;'>$status</label>";
+              } elseif ($status == "Cancelled") {
+                echo "<label style='color: red;'>$status</label>";
+              }
+              ?>
+            </td>
+
+            <td><?php echo $customer_name; ?></td>
+            <td><?php echo $customer_contact; ?></td>
+            <td><?php echo $customer_email; ?></td>
+            <td><?php echo $customer_address; ?></td>
+            <td>
+              <a href="<?php echo SITEURL; ?>admin/update-order.php?id=<?php echo $id; ?>" class="btn-secondary">Update Order</a>
             </td>
           </tr>
-          <tr>
-            <td>2.</td>
-            <td>Sam</td>
-            <td>sam</td>
-            <td>
-              <a href="" class="btn-secondary">Update Admin</a>
-              <a href="" class="btn-danger">Delete Admin</a>
-            </td>
-          </tr>
-          <tr>
-            <td>3.</td>
-            <td>Sam</td>
-            <td>sam</td>
-            <td>
-              <a href="" class="btn-secondary">Update Admin</a>
-              <a href="" class="btn-danger">Delete Admin</a>
-            </td>
-          </tr>
-        </table>
-      </div>
-    </div>
+
+      <?php
+
+        }
+      } else {
+        //Order not Available
+        echo "<tr><td colspan='12' class='error'>Orders not Available</td></tr>";
+      }
+      ?>
+
+
+    </table>
   </div>
-</section>
-<?php
-include('partials/footer.php');
-?>
+
+</div>
+
+<?php include('partials/footer.php'); ?>
