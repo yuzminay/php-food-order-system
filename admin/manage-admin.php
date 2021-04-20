e2e33be7bd98365468aeea8ad643a58ed36d9080
samir <samirbabayevdir@gmail.com>
2021-04-20 16:45:10 +0400

    displaying admins in manage-admin

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -49,33 +49,56 @@ include('partials/menu.php');
             <th>Username</th>
             <th>Actions</th>
           </tr>
-          <tr>
-            <td>1.</td>
-            <td>Sam</td>
-            <td>sam</td>
-            <td>
-              <a href="" class="btn-secondary">Update Admin</a>
-              <a href="" class="btn-danger">Delete Admin</a>
-            </td>
-          </tr>
-          <tr>
-            <td>2.</td>
-            <td>Sam</td>
-            <td>sam</td>
-            <td>
-              <a href="" class="btn-secondary">Update Admin</a>
-              <a href="" class="btn-danger">Delete Admin</a>
-            </td>
-          </tr>
-          <tr>
-            <td>3.</td>
-            <td>Sam</td>
-            <td>sam</td>
-            <td>
-              <a href="" class="btn-secondary">Update Admin</a>
-              <a href="" class="btn-danger">Delete Admin</a>
-            </td>
-          </tr>
+
+          <?php
+          //Query to Get all Admin
+          $sql = "SELECT * FROM admin";
+          //Execute the Query
+          $res = mysqli_query($conn, $sql);
+
+          //CHeck whether the Query is Executed of Not
+          if ($res == TRUE) {
+            // Count Rows to CHeck whether we have data in database or not
+            $count = mysqli_num_rows($res); // Function to get all the rows in database
+
+            $sn = 1; //Create a Variable and Assign the value
+
+            //CHeck the num of rows
+            if ($count > 0) {
+              //WE HAve data in database
+              while ($rows = mysqli_fetch_assoc($res)) {
+                //Using While loop to get all the data from database.
+                //And while loop will run as long as we have data in database
+
+                //Get individual DAta
+                $id = $rows['id'];
+                $full_name = $rows['full_name'];
+                $username = $rows['username'];
+
+                //Display the Values in our Table
+          ?>
+
+                <tr>
+                  <td><?php echo $sn++; ?>. </td>
+                  <td><?php echo $full_name; ?></td>
+                  <td><?php echo $username; ?></td>
+                  <td>
+                    <a href="<?php echo SITEURL; ?>admin/update-password.php?id=<?php echo $id; ?>" class="btn-primary">Change Password</a>
+                    <a href="<?php echo SITEURL; ?>admin/update-admin.php?id=<?php echo $id; ?>" class="btn-secondary">Update Admin</a>
+                    <a href="<?php echo SITEURL; ?>admin/delete-admin.php?id=<?php echo $id; ?>" class="btn-danger">Delete Admin</a>
+                  </td>
+                </tr>
+
+          <?php
+
+              }
+            } else {
+              //We Do not Have Data in Database
+            }
+          }
+
+          ?>
+
         </table>
       </div>
     </div>
