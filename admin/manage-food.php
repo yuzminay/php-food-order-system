f6798476dc7c630d5579c5beee8927ddc6779d6d
samir <samirbabayevdir@gmail.com>
2021-04-20 19:59:17 +0400

    admin/ food - manage, add, update, delete

--- admin/manage-food.php
+++ admin/manage-food.php
@@ -1,52 +1,120 @@
-<?php
-include('partials/menu.php');
-?>
-<section class="menu content">
+<?php include('partials/menu.php'); ?>
+
+<div class="main-content">
   <div class="wrapper">
-    <div class="main-content">
-      <div class="wrapper">
-        <h1>Manage Food</h1>
-        <br><br>
-        <a href="" class="btn-primary">Add Food</a>
-        <table class="tbl-full">
+    <h1>Manage Food</h1>
+
+    <br /><br />
+
+    <!-- Button to Add Admin -->
+    <a href="<?php echo SITEURL; ?>admin/add-food.php" class="btn-primary">Add Food</a>
+
+    <br /><br /><br />
+
+    <?php
+    if (isset($_SESSION['add'])) {
+      echo $_SESSION['add'];
+      unset($_SESSION['add']);
+    }
+
+    if (isset($_SESSION['delete'])) {
+      echo $_SESSION['delete'];
+      unset($_SESSION['delete']);
+    }
+
+    if (isset($_SESSION['upload'])) {
+      echo $_SESSION['upload'];
+      unset($_SESSION['upload']);
+    }
+
+    if (isset($_SESSION['unauthorize'])) {
+      echo $_SESSION['unauthorize'];
+      unset($_SESSION['unauthorize']);
+    }
+
+    if (isset($_SESSION['update'])) {
+      echo $_SESSION['update'];
+      unset($_SESSION['update']);
+    }
+
+    ?>
+
+    <table class="tbl-full">
+      <tr>
+        <th>S.N.</th>
+        <th>Title</th>
+        <th>Price</th>
+        <th>Image</th>
+        <th>Featured</th>
+        <th>Active</th>
+        <th>Actions</th>
+      </tr>
+
+      <?php
+      //Create a SQL Query to Get all the Food
+      $sql = "SELECT * FROM food";
+
+      //Execute the qUery
+      $res = mysqli_query($conn, $sql);
+
+      //Count Rows to check whether we have foods or not
+      $count = mysqli_num_rows($res);
+
+      //Create Serial Number VAriable and Set Default VAlue as 1
+      $sn = 1;
+
+      if ($count > 0) {
+        //We have food in Database
+        //Get the Foods from Database and Display
+        while ($row = mysqli_fetch_assoc($res)) {
+          //get the values from individual columns
+          $id = $row['id'];
+          $title = $row['title'];
+          $price = $row['price'];
+          $image_name = $row['image'];
+          $featured = $row['featured'];
+          $active = $row['active'];
+      ?>
+
           <tr>
-            <th>S.N</th>
-            <th>Full Name</th>
-            <th>Username</th>
-            <th>Actions</th>
-          </tr>
-          <tr>
-            <td>1.</td>
-            <td>Sam</td>
-            <td>sam</td>
+            <td><?php echo $sn++; ?>. </td>
+            <td><?php echo $title; ?></td>
+            <td>$<?php echo $price; ?></td>
             <td>
-              <a href="" class="btn-secondary">Update Admin</a>
-              <a href="" class="btn-danger">Delete Admin</a>
+              <?php
+              //CHeck whether we have image or not
+              if ($image_name == "") {
+                //WE do not have image, DIslpay Error Message
+                echo "<div class='error'>Image not Added.</div>";
+              } else {
+                //WE Have Image, Display Image
+              ?>
+                <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>" width="100px">
+              <?php
+              }
+              ?>
+            </td>
+            <td><?php echo $featured; ?></td>
+            <td><?php echo $active; ?></td>
+            <td>
+              <a href="<?php echo SITEURL; ?>admin/update-food.php?id=<?php echo $id; ?>" class="btn-secondary">Update Food</a>
+              <a href="<?php echo SITEURL; ?>admin/delete-food.php?id=<?php echo $id; ?>&image_name=<?php echo $image_name; ?>" class="btn-danger">Delete Food</a>
             </td>
           </tr>
-          <tr>
-            <td>2.</td>
-            <td>Sam</td>
-            <td>sam</td>
-            <td>
-              <a href="" class="btn-secondary">Update Admin</a>
-              <a href="" class="btn-danger">Delete Admin</a>
-            </td>
-          </tr>
-          <tr>
-            <td>3.</td>
-            <td>Sam</td>
-            <td>sam</td>
-            <td>
-              <a href="" class="btn-secondary">Update Admin</a>
-              <a href="" class="btn-danger">Delete Admin</a>
-            </td>
-          </tr>
-        </table>
-      </div>
-    </div>
+
+      <?php
+        }
+      } else {
+        //Food not Added in Database
+        echo "<tr> <td colspan='7' class='error'> Food not Added Yet. </td> </tr>";
+      }
+
+      ?>
+
+
+    </table>
   </div>
-</section>
-<?php
-include('partials/footer.php');
-?>
+
+</div>
+
+<?php include('partials/footer.php'); ?>
